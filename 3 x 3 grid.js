60271764c84365c6d83a2d14bf31a37ee2c84b64
Patrick0601 <100844354+Patrik0601@users.noreply.github.com>
2024-11-04 17:24:10 +0100

    Add files via upload

---
+++ 3 x 3 grid.js
@@ -0,0 +1,24 @@
+const eredetiSzamok = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function szamokKiirasa(szamok) {
+    const grid = document.getElementById("szamGrid");
+    grid.innerHTML = '';
+    szamok.forEach(szam => {
+        const div = document.createElement("div");
+        div.textContent = szam;
+        grid.appendChild(div);
+    });
+}
+
+function szamKeveres() {
+    const kevertSzamok = [...eredetiSzamok];
+    for (let i = kevertSzamok.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [kevertSzamok[i], kevertSzamok[j]] = [kevertSzamok[j], kevertSzamok[i]];
+    }
+    szamokKiirasa(kevertSzamok);
+}
+
+document.addEventListener("", () => {
+    szamokKiirasa(eredetiSzamok);
+});
